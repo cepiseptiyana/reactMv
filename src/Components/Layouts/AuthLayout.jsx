8adeda0dd2950307f0c51c4d9db9f34635440452
cepiseptiyana <cepifams3@gmail.com>
2025-05-18 21:57:12 +0700

    style(auth):menambah style cursor animation

--- src/Components/Layouts/AuthLayout.jsx
+++ src/Components/Layouts/AuthLayout.jsx
@@ -1,7 +1,51 @@
+import { useEffect, useRef } from "react";
+
 function Auth(props) {
   const { children } = props;
+  const auth = useRef(null);
+  const bubble = useRef(null);
 
-  return <>{children}</>;
+  function getColorRandom() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+
+  let lastBubbleTime = 0;
+  const delay = 80;
+
+  function handleMouseMove(e) {
+    const { pageX, pageY } = e;
+
+    const now = Date.now();
+    // cek waktu yang berlalu
+    if (now - lastBubbleTime < delay) return; // lewati jika belum cukup waktu
+    lastBubbleTime = now;
+
+    const bubble = document.createElement("div");
+    bubble.classList.add("bubble");
+    bubble.style.backgroundColor = getColorRandom();
+
+    bubble.style.left = `${pageX}px`;
+    bubble.style.top = `${pageY}px`;
+
+    auth.current.appendChild(bubble);
+
+    setTimeout(() => {
+      bubble.remove();
+    }, 1000);
+  }
+
+  return (
+    <>
+      <div ref={auth} onMouseMove={handleMouseMove}>
+        <div ref={bubble}></div>
+        {children}
+      </div>
+    </>
+  );
 }
 
 export default Auth;
